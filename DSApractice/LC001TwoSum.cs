f772a240d0c3a73c9f0e1e3cf27fa84706c108e6
Jaydeep Dave <jaydeep.dave05@hotmail.com>
2025-01-26 19:20:51 +0530

    Add project files.

---
+++ DSApractice/LC001TwoSum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSApractice
+{
+    public class LC001TwoSum
+    {   
+        public int[] TwoSumFunc(int[] nums, int target)
+        {
+            //Declare HashTable
+            Dictionary<int, int> ht = new Dictionary<int, int>();
+
+            //Loop through arr
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int tmp = nums[i];
+
+                if (ht.ContainsKey(target - tmp))
+                {
+                    return new int[] { ht[target - tmp], i };
+                }
+
+                ht[tmp] = i;
+            }
+
+            return new int[0];
+        }
+
+        public static void Main()
+        {
+            LC001TwoSum ts = new LC001TwoSum();
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] result = ts.TwoSumFunc(nums, target);
+            Console.WriteLine("[" + nums[0] + "," + nums[1] + "]");
+
+        }
+    }
+}
