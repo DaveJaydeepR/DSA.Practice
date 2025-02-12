28241d0c760de9a43e8aa80c082b255374dda485
Jaydeep Dave <jaydeep.dave05@hotmail.com>
2025-02-12 16:59:05 +0530

    ADD--LC200 and LC286

---
+++ DSApractice/LC200NumberOfIslands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Practice
+{
+    internal class LC200NumberOfIslands
+    {
+        public int NumIslands(char[][] grid)
+        {
+
+            var islands = 0;
+
+            //traverse the grid one by one
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    //if grid value is '1', increment the count, and do DFS on the adjacent 4 cells
+                    if (grid[i][j] == '1')
+                    {
+                        DFS(grid, i, j);
+                        islands++;
+                    }
+                }
+            }
+            //return the island count
+            return islands;
+        }
+
+        private void DFS(char[][] grid, int i, int j)
+        {
+            //include the cases where the traversal should be stopped
+            if (i < 0 || i >= grid.Length || j < 0 || j >= grid[0].Length || grid[i][j] == '0')
+            {
+                return;
+            }
+
+            //mark the element as visited, so that if it visited again due to DFS we won't take it to account
+            grid[i][j] = '0';
+
+            //DFS for up element
+            DFS(grid, i - 1, j);
+            //DFS for down element
+            DFS(grid, i + 1, j);
+            //DFS for left element
+            DFS(grid, i, j - 1);
+            //DFS for right element
+            DFS(grid, i, j + 1);
+        }
+    }
+}
---
+++ DSApractice/LC286WallAndGates.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Practice
+{
+    internal class LC286aWallAndGates
+    {
+        int wall = -1;
+        int Gate = 0;
+        int empty = 2147483647;
+        int[][] direction = [[-1, 0],[0, 1],[1, 0],[0, -1]];
+
+        public int[][] WallAndGates(int[][] rooms)
+        {
+            
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                for(int j = 0; j < rooms[i].Length; j++)
+                {
+                    if (rooms[i][j] == Gate)
+                    {
+                        DFS(rooms, i, j, 0);
+                    }
+                }
+            }
+
+            return rooms;
+        }
+
+        private void DFS(int[][] rooms,int row, int col,int currentPosition) 
+        { 
+        
+            if(row < 0|| row >= rooms.Length|| col < 0|| col >= rooms[0].Length|| currentPosition > rooms[row][col])
+            {
+                return;
+            }
+            rooms[row][col] = currentPosition;
+            for (int i = 0; i < direction.Length; i++)
+            {
+                int[] currentDir = direction[i];
+                DFS(rooms,row + currentDir[0], col + currentDir[1], currentPosition+1);
+            }
+
+        }
+    }
+}
