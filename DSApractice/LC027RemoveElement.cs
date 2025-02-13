223ce421e9e2a3b02bcb728a8d2553b1228bba36
Jaydeep Dave <jaydeep.dave05@hotmail.com>
2025-02-13 18:06:16 +0530

    LC027 and LC080 Added

---
+++ DSApractice/LC027RemoveElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Practice
+{
+    internal class LC027RemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length <= 1) { return nums.Length; }
+            var index = 0;
+            var lastValue = nums[index];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == lastValue) { continue; }
+
+                nums[++index] = nums[i];
+                lastValue = nums[index];
+            }
+            return index + 1;
+        }
+    }
+}
---
+++ DSApractice/LC080RemoveSecondDuplicate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Practice
+{
+    internal class LC080RemoveSecondDuplicate
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2) // Check if the array has fewer than 2 elements
+            {
+                return nums.Length; // Return the length of the array as no duplicates exist
+            }
+            int k = 2;
+            for (int i = 2; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[k - 2])
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+
+            return k;
+        }
+    }
+}
